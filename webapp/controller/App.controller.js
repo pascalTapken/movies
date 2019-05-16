69d0fcaf6a4427b2e61490d6b8fdd373eeb6e75b
pascalTapken <pascal.tapken@zeit-consulting.de>
2019-05-16 11:05:12 +0000

    feat: unit2 implement.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,30 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/base/Log"
+], function (Controller, Log) {
 	"use strict";
 
 	return Controller.extend("opensap.movies.controller.App", {
 		onInit: function () {
-
+			Log.info("Controller has been initialized!");
+		},
+		
+		onBeforeRendering: function(){
+			Log.info("The view will shortly be rendered!");
+		},
+		
+		onAfterRendering: function(){
+			Log.info("The view has been rendered!");
+		},
+		
+		onPress: function(sValue){
+			sap.ui.require(["sap/m/MessageToast"], function(oMessage){
+				oMessage.show("Searching… " + sValue);
+			});
+		},
+		
+		onExit: function(){
+			Log.info("Controller will shortly be destroyed!");
 		}
 	});
 });
